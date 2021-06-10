86bcf007012f29d2886521b612cf3b4eb439b3dc
krobus00 <michaelglaxy@gmail.com>
2021-06-11 01:03:23 +0700

    🐛 fix subnetting

--- main.py
+++ main.py
@@ -35,15 +35,15 @@ class NetworkTopo(Topo):
 
     def build(self, **_opts):
         # add router r1-r4
-        r1 = self.addNode('r1', cls=LinuxRouter, ip='192.168.1.1/24')
-        r2 = self.addNode('r2', cls=LinuxRouter, ip='192.168.3.1/24')
-        r3 = self.addNode('r3', cls=LinuxRouter, ip='192.168.1.2/24')
-        r4 = self.addNode('r4', cls=LinuxRouter, ip='192.168.3.2/24')
+        r1 = self.addNode('r1', cls=LinuxRouter, ip='10.17.2.1/30')
+        r2 = self.addNode('r2', cls=LinuxRouter, ip='10.17.3.1/30')
+        r3 = self.addNode('r3', cls=LinuxRouter, ip='10.17.2.2/30')
+        r4 = self.addNode('r4', cls=LinuxRouter, ip='10.17.3.2/30')
         # add host ha & hb
-        ha = self.addHost('ha', ip='192.168.10.2/24',
-                          defaultRoute='via 192.168.10.1')
-        hb = self.addHost('hb', ip='192.168.17.2/24',
-                          defaultRoute='via 192.168.17.1')
+        ha = self.addHost('ha', ip='10.17.0.2/30',
+                          defaultRoute='via 10.17.0.1')
+        hb = self.addHost('hb', ip='10.17.4.2/30',
+                          defaultRoute='via 10.17.4.1')
         # add links
 
         # router - router
@@ -56,23 +56,23 @@ class NetworkTopo(Topo):
                          max_queue_size=max_size, use_tbf=True)
 
         self.addLink(r1, r3, cls=TCLink, **linkopts0, intfName1='r1-eth1', intfName2='r3-eth1',
-                     params1={'ip': '192.168.1.1/24'}, params2={'ip': '192.168.1.2/24'})
+                     params1={'ip': '10.17.2.1/30'}, params2={'ip': '10.17.2.2/30'})
         self.addLink(r1, r4, cls=TCLink, **linkopts1, intfName1='r1-eth2', intfName2='r4-eth1',
-                     params1={'ip': '192.168.2.1/24'}, params2={'ip': '192.168.2.2/24'})
+                     params1={'ip': '10.17.6.1/30'}, params2={'ip': '10.17.6.2/30'})
         self.addLink(r2, r4, cls=TCLink, **linkopts0, intfName1='r2-eth1', intfName2='r4-eth2',
-                     params1={'ip': '192.168.3.1/24'}, params2={'ip': '192.168.3.2/24'})
+                     params1={'ip': '10.17.3.1/30'}, params2={'ip': '10.17.3.2/30'})
         self.addLink(r2, r3, cls=TCLink, **linkopts1, intfName1='r2-eth2', intfName2='r3-eth2',
-                     params1={'ip': '192.168.4.1/24'}, params2={'ip': '192.168.4.2/24'})
+                     params1={'ip': '10.17.7.1/30'}, params2={'ip': '10.17.7.2/30'})
         # router - host
         self.addLink(ha, r1, cls=TCLink, **linkopts1,  intfName2='r1-eth3',
-                     params1={'ip': '192.168.10.2/24'}, params2={'ip': '192.168.10.1/24'})
+                     params1={'ip': '10.17.0.2/30'}, params2={'ip': '10.17.0.1/30'})
         self.addLink(ha, r2, cls=TCLink, **linkopts1, intfName2='r2-eth3',
-                     params1={'ip': '192.168.11.2/24'}, params2={'ip': '192.168.11.1/24'})
+                     params1={'ip': '10.17.1.2/30'}, params2={'ip': '10.17.1.1/30'})
 
         self.addLink(hb, r3, cls=TCLink, **linkopts1, intfName2='r3-eth3',
-                     params1={'ip': '192.168.17.2/24'}, params2={'ip': '192.168.17.1/24'})
+                     params1={'ip': '10.17.4.2/30'}, params2={'ip': '10.17.4.1/30'})
         self.addLink(hb, r4, cls=TCLink, **linkopts1, intfName2='r4-eth3',
-                     params1={'ip': '192.168.18.2/24'}, params2={'ip': '192.168.18.1/24'})
+                     params1={'ip': '10.17.5.2/30'}, params2={'ip': '10.17.5.1/30'})
 
 
 def run():
@@ -91,37 +91,37 @@ def run():
             router.waitOutput()
             print("Starting zebra and rip on {}".format(router.name))
     # MPTCP ROUTING IMPLEMENTATION
-    net['ha'].cmd("ip rule add from 192.168.10.2 table 1")
-    net['ha'].cmd("ip rule add from 192.168.11.2 table 2")
+    net['ha'].cmd("ip rule add from 10.17.0.2 table 1")
+    net['ha'].cmd("ip rule add from 10.17.1.2 table 2")
     net['ha'].cmd(
-        "ip route add 192.168.10.0/24 dev ha-eth0 scope link table 1")
-    net['ha'].cmd("ip route add default via 192.168.10.1 dev ha-eth0 table 1")
+        "ip route add 10.17.0.0/30 dev ha-eth0 scope link table 1")
+    net['ha'].cmd("ip route add default via 10.17.0.1 dev ha-eth0 table 1")
     net['ha'].cmd(
-        "ip route add 192.168.11.0/24 dev ha-eth1 scope link table 2")
-    net['ha'].cmd("ip route add default via 192.168.11.1 dev ha-eth1 table 2")
+        "ip route add 10.17.1.0/30 dev ha-eth1 scope link table 2")
+    net['ha'].cmd("ip route add default via 10.17.1.1 dev ha-eth1 table 2")
 
     net['ha'].cmd(
-        "ip route add default scope global nexthop via 192.168.10.1 dev ha-eth0")
+        "ip route add default scope global nexthop via 10.17.0.1 dev ha-eth0")
 
-    net['hb'].cmd("ip rule add from 192.168.17.2 table 1")
-    net['hb'].cmd("ip rule add from 192.168.18.2 table 2")
+    net['hb'].cmd("ip rule add from 10.17.4.2 table 1")
+    net['hb'].cmd("ip rule add from 10.17.5.2 table 2")
     net['hb'].cmd(
-        "ip route add 192.168.17.0/24 dev hb-eth0 scope link table 1")
-    net['hb'].cmd("ip route add default via 192.168.17.1 dev hb-eth0 table 1")
+        "ip route add 10.17.4.0/30 dev hb-eth0 scope link table 1")
+    net['hb'].cmd("ip route add default via 10.17.4.1 dev hb-eth0 table 1")
     net['hb'].cmd(
-        "ip route add 192.168.18.0/24 dev hb-eth1 scope link table 2")
-    net['hb'].cmd("ip route add default via 192.168.18.1 dev hb-eth1 table 2")
+        "ip route add 10.17.5.0/30 dev hb-eth1 scope link table 2")
+    net['hb'].cmd("ip route add default via 10.17.5.1 dev hb-eth1 table 2")
 
     net['hb'].cmd(
-        "ip route add default scope global nexthop via 192.168.17.1 dev hb-eth0")
+        "ip route add default scope global nexthop via 10.17.4.1 dev hb-eth0")
     time.sleep(5)
     print("*** Connection test")
     net.pingAll()
     print("*** Bandwidth test")
     time.sleep(5)
-    net['ha'].cmd('iperf -s &')
+    net['ha'].cmd('iperf -s -i 1 &')
     time.sleep(1)
-    net['hb'].cmdPrint('iperf -c 192.168.10.2 -i 1')
+    net['hb'].cmdPrint('iperf -c 10.17.0.2 -i 1')
     CLI(net)
     net.stop()
     os.system("killall -9 zebra ripd")
